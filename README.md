ff15cbb7eaf777425b1a0598d25abb4b4e6024ed
GrokTek-Local <GrokTek-Local@LT-PV-0117.BIZNECTED.LOCAL>
2023-06-12 16:31:38 -0700

    First actual commit with coding and README changes (practice)

--- README.md
+++ README.md
@@ -3,4 +3,13 @@
 ## How does it work?
 
 1. The program will first generate a random number.
-2. There is an object with 3 keys
+2. There is an object with 2 keys that
+
+## Why I did this project:
+
+1. To practice my JS syntax.
+2. To practice using the command line along with GitHub and VS Code.
+
+## Contributors:
+
+1. @KyleRedfield
--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // random number generator
 function randomNumberGenerator(num) {
     // num is defined by .length of array below
-    return Math.random(Math.floor() * num)
+    return Math.floor(Math.random() * num)
 }
 
+// object that holds all of the words or phrases that random are selected
+const wordsOfEncouragment = {
+    youWill: [],
+    youAre: [],
+}
