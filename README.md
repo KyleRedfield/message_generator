6b5d474ce9a69e841a8d1948e67007daeb7fa5a8
GrokTek-Local <GrokTek-Local@LT-PV-0117.BIZNECTED.LOCAL>
2023-06-12 16:16:31 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Positive Words of Encouragment
+
+## How does it work?
+
+1. The program will first generate a random number.
+2. There is an object with 3 keys
---
+++ script.js
@@ -0,0 +1,6 @@
+// random number generator
+function randomNumberGenerator(num) {
+    // num is defined by .length of array below
+    return Math.random(Math.floor() * num)
+}
+
