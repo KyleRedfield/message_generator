9b98ae2a3685c3374e6abcb0c023c60d7d3a7bef
GrokTek-Local <GrokTek-Local@LT-PV-0117.BIZNECTED.LOCAL>
2023-06-13 08:39:24 -0700

    completed script.js

--- script.js
+++ script.js
@@ -5,28 +5,35 @@ function randomNumberGenerator(num) {
 };
 
 // object that holds all of the words or phrases that are randomly selected
-const wordsOfEncouragment = {
+const wordsOfEncouragement = {
     youWill: ['be amazing today', 'do great things today', 'accomplish all of your goals', 'overcome any obstacles in front of you'],
     youAre: ['incredible', 'amazing', 'intelligent', 'beautiful', 'caring', 'unique'],
     youCan: ['accomplish anything', 'accomplish whatever you set your mind out to do', 'turn your dreams into a reality', 'embrace change and adapt to new situations']
 };
 
-// storage for the words of encouragment creating a new array
+// storage for the words of encouragement creating a new array
 let encouragementArray = [];
 
 // loop through the object to select a random index using the random number function and .length
-for (let words in wordsOfEncouragment) {
-    let randomIndex = randomNumberGenerator(wordsOfEncouragment[words].length)
+for (let words in wordsOfEncouragement) {
+    let randomIndex = randomNumberGenerator(wordsOfEncouragement[words].length)
 
     switch (words) {
         case 'youWill':
-            encouragementArray.push(`You Will: ${wordsOfEncouragment[words][randomIndex]}`);
+            encouragementArray.push(`You Will: ${wordsOfEncouragement[words][randomIndex]}`);
             break;
         case 'youAre':
-            encouragementArray.push(`You Are: ${wordsOfEncouragment[words][randomIndex]}`);
+            encouragementArray.push(`You Are: ${wordsOfEncouragement[words][randomIndex]}`);
             break;
         case 'youCan':
-            encouragementArray.push(`You Can: ${wordsOfEncouragment[words][randomIndex]}`);
+            encouragementArray.push(`You Can: ${wordsOfEncouragement[words][randomIndex]}`);
             break;
     };
 }
+
+function encouragementJoin(encouragement) {
+    const joined = encouragementArray.join('\n');
+    console.log(joined);
+}
+
+encouragementJoin(encouragementArray);
